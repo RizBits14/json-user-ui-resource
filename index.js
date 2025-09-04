c00cf54ef0188d42b745a887bd5b251637e9c925
RizBits14 <mohammad.rizwanul.islam12014@gmail.com>
2025-09-05 04:27:45 +0600

    console logged the elements which i will need

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ const loadPost = () => {
     .then(resource => resource.json())
     .then(json => displayPost(json))
 }
+
+const displayPost = (posts) => { 
+    posts.forEach((post) => { 
+        const {name, id, email, username, address} = post
+        const {street, geo} = address
+        const {lat, lng} = geo
+        console.log(name, id, email, username, street, lat, lng)
+    })
+}
